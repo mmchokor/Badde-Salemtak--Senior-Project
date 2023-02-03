be68c6730a66f9dc92b23fab181f98c3a18fde35
KaramT <83702069+KaramTimani@users.noreply.github.com>
2023-02-03 18:44:08 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up</Text>
+  
+      <Text>Open Here</Text>
+       <Text>Open There</Text>
       <StatusBar style="auto" />
     </View>
   );
