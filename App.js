545a32a60c8e3f9c8d61a5c4a7fa2f9859a51cd5
KaramT <83702069+KaramTimani@users.noreply.github.com>
2023-02-03 18:54:16 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,44 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-  
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-       <Text>Open There</Text>
-
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-       <Text>Open There</Text>
-
-       <Text>test pull vs fetch</Text>
-       <Text>test pull vs fetch</Text>
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-       <Text>Open There</Text>
-
-       <Text>test pull vs fetch</Text>
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-       <Text>Open There</Text>
-
-       <Text>test pull vs fetch</Text>
-      <Text>Open Here</Text>
-       <Text>Open There</Text>
-
-       <Text>Open There</Text>
-
-       <Text>test pull vs fetch</Text>
-       <Text>Open There</Text>
-
-       <Text>test pull vs fetch</Text>
-
       <StatusBar style="auto" />
     </View>
   );
