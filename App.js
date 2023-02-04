1c180fd68a88a751d90359e87a7deee61a7dd394
Karam Timani <sss484376@gmail.com>
2023-02-04 12:58:52 +0200

    More edits and changes on home screen and the navigation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -20,89 +20,125 @@ const BottomTabs = createBottomTabNavigator();
 
 function BottomBar() {
 	return (
-		<BottomTabs.Navigator
-			screenOptions={({ navigation }) => ({
-				headerStyle: { backgroundColor: Colors.white },
-				headerTintColor: "white",
-				tabBarStyle: { backgroundColor: Colors.white,paddingTop:4 },
-				tabBarActiveTintColor: Colors.darkGreen,
-			})}
-		>
-			<BottomTabs.Screen
-				name='Home'
-				component={HomeTravelerScreen}
-				options={{
-					title: "",
-					
-					tabBarIcon: ({ color }) => (
-						<MaterialIcons  name='home' size={35} color={color} />
-					),
-					headerRight: () => (
-						<Feather name='message-circle' size={35} color={Colors.darkGreen} />
-					),
-					headerLeft: () => (
-						<Ionicons name='ios-filter' size={35} color={Colors.darkGreen} />
-					),
-				}}
-			/>
-			<BottomTabs.Screen
-				name='Favorites'
-				component={FavoritesScreen}
-				options={{
-					title: "",
+		<View style={{ flexDirection: "row", flex: 1 }}>
+			<BottomTabs.Navigator
+				screenOptions={({ navigation }) => ({
+					headerStyle: { backgroundColor: Colors.white },
+					headerTintColor: "black",
+					tabBarStyle: {
+						backgroundColor: Colors.white,
+						height: 48,
+						flexDirection: "row",
+						justifyContent: "center",
+					},
+					tabBarActiveTintColor: Colors.darkGreen,
+				})}
+			>
+				<BottomTabs.Screen
+					name='Home'
+					component={HomeTravelerScreen}
+					options={{
+						title: "",
 
-					tabBarIcon: ({ color, size }) => (
-						<Fontisto name='favorite' size={size} color={color} />
-					),
-				}}
-			/>
-			<BottomTabs.Screen
-				name='Add'
-				component={AddScreen}
-				options={{
-					title: "",
+						tabBarIcon: ({ color }) => (
+							<MaterialIcons name='home' size={35} color={color} />
+						),
+						headerRight: () => (
+							<View style={{ flexDirection: "row-reverse" }}>
+								<Feather
+									name='message-circle'
+									size={35}
+									color={Colors.darkGreen}
+									style={{ paddingRight: 5 }}
+								/>
+								<View
+									style={{
+										flexDirection: "row",
+										marginRight: 15,
+										borderColor: Colors.darkGreen,
+										borderWidth: 2,
+										borderRadius: 20,
+										paddingLeft: 18,
+										paddingRight:3,
+									}}
+								>
+									<TextInput style={{backgroundColor:Colors.grayBackground,}} inlineImageLeft='search_icon' />
+									<Ionicons name='search-outline' size={32} color={Colors.darkGreen} />
+								</View>
+							</View>
+						),
+						headerLeft: () => (
+							<Ionicons
+								name='ios-filter'
+								size={35}
+								color={Colors.darkGreen}
+								style={{ paddingLeft: 5 }}
+							/>
+						),
+					}}
+				/>
+				<BottomTabs.Screen
+					name='Favorites'
+					component={FavoritesScreen}
+					options={{
+						title: "",
 
-					tabBarIcon: ({ color, size }) => (
-						<MaterialIcons name='add' size={40} color={color} />
-					),
-				}}
-			/>
-			<BottomTabs.Screen
-				name='Notifications'
-				component={NotificationsScreen}
-				options={{
-					title: "",
+						tabBarIcon: ({ color, size }) => (
+							<Fontisto name='favorite' size={size} color={color} />
+						),
+					}}
+				/>
+				<BottomTabs.Screen
+					name='Add'
+					component={AddScreen}
+					options={{
+						title: "",
 
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name='notifications' size={size} color={color} />
-					),
-				}}
-			/>
-			<BottomTabs.Screen
-				name='Profile'
-				component={ProfileScreen}
-				options={{
-					title: "",
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons name='ios-add-circle' size={35} color={color} />
+						),
+					}}
+				/>
+				<BottomTabs.Screen
+					name='Notifications'
+					component={NotificationsScreen}
+					options={{
+						title: "",
 
-					tabBarIcon: ({ color, size }) => (
-						<Ionicons name='person' size={size} color={color} />
-					),
-				}}
-			/>
-		</BottomTabs.Navigator>
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons name='notifications' size={30} color={color} />
+						),
+					}}
+				/>
+				<BottomTabs.Screen
+					name='Profile'
+					component={ProfileScreen}
+					options={{
+						title: "",
+
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons name='person' size={size} color={color} />
+						),
+					}}
+				/>
+			</BottomTabs.Navigator>
+		</View>
 	);
 }
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen
-					name='Homee'
-					component={BottomBar}
-					options={{ headerShown: false }}
-				/>
-			</Stack.Navigator>
-		</NavigationContainer>
+		<>
+			<StatusBar style='dark' />
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen
+						name='Homee'
+						component={BottomBar}
+						options={{ headerShown: false }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
+		</>
 	);
 }
 
