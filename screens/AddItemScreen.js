bc3a4208c0468c1ae03fe90885a9d5a4212217aa
Karam Timani <sss484376@gmail.com>
2023-02-10 23:54:38 +0200

    Worked on the add screen

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -2,57 +2,88 @@ import { StyleSheet, Text, TextInput, View, Pressable } from "react-native";
 import { Colors } from "../constants/colors";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
+import BorderStyle from "../components/UI/BorderStyle";
 function AddItemScreen({ navigation }) {
 	function PressEventHandler() {
 		navigation.navigate("Location");
 	}
 	return (
-		<View style={{ alignItems: "center" }}>
-			<View style={styles.upperButton}>
-				<View style={styles.item}>
-					<Text style={styles.textI}>Add Item</Text>
-				</View>
-				<Pressable onPress={PressEventHandler}>
-					<View style={styles.location}>
-						<Text style={styles.textL}>Add Location</Text>
+		<View style={{ backgroundColor: Colors.white }}>
+			<View style={{ alignItems: "center" }}>
+				<View style={styles.upperButton}>
+					<View style={styles.item}>
+						<Text style={styles.textI}>Add Item</Text>
 					</View>
-				</Pressable>
+					<Pressable onPress={PressEventHandler}>
+						<View style={styles.location}>
+							<Text style={styles.textL}>Add Location</Text>
+						</View>
+					</Pressable>
+				</View>
 			</View>
 			<ScrollView>
-				<View>
-					<Text>Item Name*</Text>
-					<TextInput placeholder='iphone 13 pro max' />
-					<Text>Price</Text>
+				<View style={{ flex: 1, paddingHorizontal: 30 }}>
+					<Text style={styles.textHead}>Item Name*</Text>
+					<TextInput placeholder='iphone 13 pro max' style={styles.inputT} />
+					<Text style={styles.textHead}>Price</Text>
 
 					{/* Price View */}
 					<View style={{ flexDirection: "row", alignItems: "center" }}>
-						<View>
-							<Text>USD</Text>
-						</View>
+						<BorderStyle>
+							<Text
+								style={{
+									fontSize: 12,
+									opacity: 0.7,
+									fontFamily: "inter-light",
+								}}
+							>
+								USD
+							</Text>
+						</BorderStyle>
 
-						<TextInput placeholder='918' />
+						<TextInput
+							placeholder='918'
+							style={[styles.inputT, { marginLeft: 10, width: 50 }]}
+							keyboardType='number-pad'
+							maxLength={5}
+						/>
 					</View>
 					{/* Quantity and Weight View */}
 					<View style={{ flexDirection: "row" }}>
 						<View>
-							<Text>Quantity</Text>
+							<Text style={styles.textHead}>Quantity</Text>
 							<View>{/* The adder *1 up or down */}</View>
 						</View>
 						<View>
-							<Text>Weight</Text>
+							<Text style={styles.textHead}>Weight</Text>
 							<View style={{ flexDirection: "row", alignItems: "center" }}>
-								<View style={{ flexDirection: "row", alignItems: "center" }}>
-									<FontAwesome5 name='weight-hanging' size={20} />
-									<Text>Kg</Text>
-								</View>
-								<TextInput placeholder='0.5' />
+								<BorderStyle>
+									<View style={{ flexDirection: "row", alignItems: "center" }}>
+										<FontAwesome5 name='weight-hanging' size={15} />
+										<Text
+											style={{
+												fontSize: 12,
+												opacity: 0.7,
+												fontFamily: "inter-light",
+											}}
+										>
+											Kg
+										</Text>
+									</View>
+								</BorderStyle>
+								<TextInput
+									placeholder='0.5'
+									style={[styles.inputT, { marginLeft: 10, width: 50 }]}
+									keyboardType='number-pad'
+									maxLength={5}
+								/>
 							</View>
 						</View>
 					</View>
 
 					{/* Type */}
 					<View>
-						<Text>Type</Text>
+						<Text style={styles.textHead}>Type</Text>
 						<View
 							style={{
 								flexWrap: "wrap",
@@ -74,9 +105,9 @@ function AddItemScreen({ navigation }) {
 
 					{/* More Details,Location */}
 					<View>
-						<Text>More Details</Text>
+						<Text style={styles.textHead}>More Details</Text>
 						<TextInput placeholder='more details' style={{ borderWidth: 1 }} />
-						<Text>Location</Text>
+						<Text style={styles.textHead}>Location</Text>
 						<Text>Address*</Text>
 						<TextInput placeholder='address' style={{ borderWidth: 1 }} />
 						<Text>Street Name*</Text>
@@ -97,9 +128,7 @@ function AddItemScreen({ navigation }) {
 							style={{ borderWidth: 1 }}
 						/>
 						<Text>Preferred Payment Method</Text>
-						<View>
-							
-						</View>
+						<View></View>
 					</View>
 				</View>
 			</ScrollView>
@@ -141,5 +170,18 @@ const styles = StyleSheet.create({
 	textL: {
 		color: Colors.white,
 		fontFamily: "inter-regular",
+	},
+	textHead: {
+		color: Colors.darkGreen,
+		fontSize: 18,
+		fontFamily: "inter-regular",
+		marginTop: 10,
+	},
+	inputT: {
+		fontFamily: "inter-regular",
+		color: Colors.black,
+		fontSize: 16,
+		borderBottomWidth: 0.5,
+		borderBottomColor: Colors.gray,
 	},
 });
